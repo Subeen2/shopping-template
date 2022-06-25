a3d9b6ac0e1ae66503eeae3733acb59f246b0d27
이수빈 <snb0768@naver.com>
2022-06-25 12:30:16 +0900

    pwa 추가

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
+// Fetch the items from the JSON file
 function loadItems() {
   return fetch('data/data.json')
     .then(response => response.json())
     .then(json => json.items);
-
 }
 
+// Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector('.items');
   container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// Create HTML list item from the given data item
 function createHTMLString(item) {
   return `
-  <li class="item">
-  <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
-  <span class="item__description">${item.gender}, ${item.size}</span>
-</li>
-  `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
 function onButtonClick(event, items) {
@@ -27,27 +29,18 @@ function onButtonClick(event, items) {
   if (key == null || value == null) {
     return;
   }
-  updateItems(items, key, value);
-  // displayItems(items.filter(item => item[key] === value));
-}
 
-function updateItems(items, key, value) {
-  items.forEach(item => {
-    if (item.dataset[key] === value) {
-      item.classList.remove('invisible');
-    } else {
-      item.classList.add('invisible');
-    }
-  })
+  displayItems(items.filter(item => item[key] === value));
 }
 
 function setEventListeners(items) {
   const logo = document.querySelector('.logo');
   const buttons = document.querySelector('.buttons');
   logo.addEventListener('click', () => displayItems(items));
-  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
+// main
 loadItems()
   .then(items => {
     displayItems(items);
